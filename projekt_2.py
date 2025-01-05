cf8a255c9bcf8410624e8b7ff6478199b263af92
Jakubb1994 <prajzler25@gmail.com>
2025-01-05 09:43:31 +0100

    řádek 9 - smazán řádek 9 - vložení funkce pro generování náhodného čísla převedení čísel na řetězce

--- projekt_2.py
+++ projekt_2.py
@@ -6,4 +6,10 @@ email: prajzler25@gmail.com
 """
 import random
 
-nahodne_cislo = random.randint(1, 4)
+def generate_secret_number():
+    # Generates a random 4-digit number with unique digits, not starting with 0
+    digits = list(range(1, 10))                  # First digit num. cannot be 0
+    first_digit = random.choice(digits)
+    digits.remove(first_digit)
+    other_digits = random.sample(digits + [0], 3)
+    return str(first_digit) + ''.join(map(str, other_digits))
