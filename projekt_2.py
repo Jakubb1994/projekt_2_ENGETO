bdb33e52ea449430e03b82e6f0b1b53f46505ea4
EngetoStudent <prajzler25@]mail.com>
2025-02-08 09:44:01 +0100

    řádek 10, 18 - změna docstring

--- projekt_2.py
+++ projekt_2.py
@@ -7,7 +7,7 @@ email: prajzler25@gmail.com
 import random
 
 def generate_secret_number():
-    # Generates a random 4-digit number with unique digits, not starting with 0
+    """Generates a random 4-digit number with unique digits, not starting with 0"""
     digits = list(range(1, 10))                  # First digit num. cannot be 0
     first_digit = random.choice(digits)
     digits.remove(first_digit)
@@ -15,7 +15,7 @@ def generate_secret_number():
     return str(first_digit) + ''.join(map(str, other_digits))
 
 def validate_guess(guess):
-    # Validates the user's guess for correct lenght, digits and uniqueness
+    """Validates the user's guess for correct lenght, digits and uniqueness"""
     if len(guess) != 4 or not guess.isdigit():
         return False, "Input must be a 4-digit number."
         if len(set(guess)) != 4:
@@ -24,4 +24,6 @@ def validate_guess(guess):
             return False, "Number cannot start with 0."
         return True, ""
         
+def evaluate_guess(secret, guess):
+    """Evalutes the guess and returns the number of bulls and cows"""
     
